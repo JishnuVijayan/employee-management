d447ec607265a0d33cfad3d2a1ba99ab0745f626
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-07 13:57:48 +0530

    Structural and attribute directives

---
+++ src/app/components/master/master.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationComponent } from '../designation/designation.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-master',
+  imports: [RolesComponent, DesignationComponent, CommonModule],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css',
+})
+export class MasterComponent {
+  currentTab: string = 'Roles';
+
+  changeTab(tabName: string) {
+    this.currentTab = tabName;
+  }
+}
