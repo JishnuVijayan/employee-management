50c38e6a25c30c122ed321b7460765f8205bf9b5
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-07 10:24:57 +0530

    Checking components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
+import { DesignationComponent } from './components/designation/designation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RolesComponent, DesignationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'employee-management';
