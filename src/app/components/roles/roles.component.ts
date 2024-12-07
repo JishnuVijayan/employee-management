5cb813f47f0fc4d8c8bc5197e8de5bf9302577aa
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-07 11:21:03 +0530

    Interpolation and property binding practice

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-roles',
   imports: [],
   templateUrl: './roles.component.html',
-  styleUrl: './roles.component.css'
+  styleUrl: './roles.component.css',
 })
 export class RolesComponent {
-
+  firstName: string = 'Hello';
+  secondName: string = 'World';
+  currentDate: Date = new Date();
+  bool: boolean = false;
 }
