610ff2bfdb138f4a2c0e5c45f39f56704759ea16
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-07 15:46:51 +0530

    Get API call for roles.

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Irole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css',
 })
-export class RolesComponent {
-  firstName: string = 'Hello';
-  secondName: string = 'World';
-  currentDate: Date = new Date();
-  bool: boolean = false;
-  selectedState: string = '';
+export class RolesComponent implements OnInit {
+  roleList: Irole[] = [];
+  http = inject(HttpClient); //instance of the HttpClient service
 
-  showWelcomeMessage() {
-    alert('Welcome');
+  ngOnInit(): void {
+    this.getRoles();
   }
-  showMessage(message: string) {
-    alert(message);
+
+  getRoles() {
+    this.http
+      .get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles')
+      .subscribe((res: any) => {
+        this.roleList = res.data;
+      });
   }
+
+  // firstName: string = 'Hello';
+  // secondName: string = 'World';
+  // currentDate: Date = new Date();
+  // bool: boolean = false;
+  // selectedState: string = '';
+
+  // showWelcomeMessage() {
+  //   alert('Welcome');
+  // }
+  // showMessage(message: string) {
+  //   alert(message);
+  // }
 }
-
-
