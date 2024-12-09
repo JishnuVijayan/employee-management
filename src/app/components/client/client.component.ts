c7d776d08ee24c3660d8f27a5a61c39ba59499e7
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-09 18:16:30 +0530

    API for employee page and to add employee

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/client';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ClientService } from '../../services/client.service';
+import { APIResponseModel } from '../../model/interface/role';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  clientObj: Client = new Client();
+  clientList: Client[] = [];
+  clientService = inject(ClientService);
 
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients() {
+    this.clientService.getAllClients().subscribe((res: APIResponseModel) => {
+      this.clientList = res.data;
+    });
+  }
+
+  onSaveClient() {
+    this.clientService
+      .addUpdate(this.clientObj)
+      .subscribe((res: APIResponseModel) => {
+        if (res.result) {
+          alert('Client created');
+          this.loadClients();
+        } else {
+          alert(res.message);
+        }
+      });
+  }
 }
