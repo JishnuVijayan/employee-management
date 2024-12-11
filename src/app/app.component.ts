a453ee1dae416baa9a2a9cbcb99c69eefad341be
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-11 18:18:55 +0530

    Client Project component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MasterComponent } from './components/master/master.component';
+
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MasterComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
